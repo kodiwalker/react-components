9dfc39893f1fb914d3a406de8aadd2fdcb0bfa15
Kodi <kodi.w13@gmail.com>
2023-04-10 12:02:59 -0600

    Completed exercise

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-// TODO
+const { useState } = React;
+
+const GroceryListItem = (props) => {
+  const [done, setDone] = useState(false);
+  const [bold, setBold] = useState(false);
+  const style = {
+    textDecoration: done ? 'line-through' : 'none',
+    fontWeight: bold ? 'bold' : 'normal'
+  };
+
+  return (
+    <li style={style} onClick={() => setDone(!done)} onMouseEnter={() => setBold(!bold)} onMouseLeave={() => setBold(!bold)}>
+      {props.item}
+    </li>
+  )
+}
+
+const GroceryList = (props) => (
+  <ul>
+    {props.items.map((item) => (
+      <GroceryListItem item={item} />
+    ))}
+  </ul>
+)
+
+
+ReactDOM.render(<GroceryList items={['Almond Milk', 'Tofu', 'Eggs', 'TVP']} />, document.getElementById('app'));
